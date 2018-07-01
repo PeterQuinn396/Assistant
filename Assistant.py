e03cf9b212ec04a2caa14599c76c521d8e378e3b
PeterQuinn396 <peter.quinn396@gmail.com>
2018-07-01 09:55:26 -0400

    test syncing to pi

--- Assistant.py
+++ Assistant.py
@@ -12,4 +12,27 @@ def main():
     recognizer.expect_phrase('computer')
 
     button = aiy.voicehat.get_button()
-    led = aiy.
+    led = aiy.voicehat.get_led()
+    aiy.audio.get_recorder().start()
+
+    while True:
+        print('Press the button and speak')
+        button.wait_for_press()
+        print('Listening...')
+        text = recognizer.recognize()
+        if not text:
+            print('Sorry, I did not hear you.')
+        else:
+            print('You said "', text, '"')
+            if 'turn on the light' in text:
+                led.set_state(aiy.voicehat.LED.ON)
+            elif 'turn off the light' in text:
+                led.set_state(aiy.voicehat.LED.OFF)
+            elif 'blink' in text:
+                led.set_state(aiy.voicehat.LED.BLINK)
+            elif 'goodbye' in text:
+                break
+
+
+if __name__ == '__main__':
+    main()
