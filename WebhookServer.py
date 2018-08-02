71002b77ead958f31e533f3c7cfc71814df50b31
Peter Quinn <peter.quinn396@gmail.com>
2018-08-01 22:17:23 -0400

    Started webhook server code
    Works when running on local network. Does not work when trying to connect from the internet. Port forwarding on router was done. Suspect issues with Windows firewall. To try on pi to see if it magically works

---
+++ WebhookServer.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST','GET'])
+def index():
+    return 'Choose Option'
+
+
+@app.route('/test_cmd', methods = ['POST','GET'])
+def test_cmd():
+    print("Test cmd done")
+    return  "Test cmd done"
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
